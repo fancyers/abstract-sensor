ee5004ec59992a9615c48b58ad58244b1e38d7dd
fancyers <kjng5901@gmail.com>
2022-02-05 10:18:36 +0900

    테스트 코드 완료

--- src/index.js
+++ src/index.js
@@ -1,6 +1,58 @@
-class Sensor {}
+class Sensor {
+    constructor(deviceId) {
+        this.deviceId = deviceId
+        this.powerStatus = 'off';
+        this.reportingInterval = 10000;
+    }
 
-class IotServer {}
+    turn(input) {
+        if (input === 'on') {
+            if (this.powerStatus === 'on') {
+                throw Error();
+            }
+            this.powerStatus = 'on';
+            this.status = 'idle';
+            setTimeout(() => {
+                this.status = 'sensingDistance';
+                setTimeout(() => {
+                    this.status = 'reportingData';
+                    setTimeout(() => {
+                        this.status = 'idle';
+                    }, 1000);
+                }, 500);
+            }, this.reportingInterval);
+        } else if (input === 'off') {
+            this.powerStatus = 'off';
+        }
+    }
+}
+
+class IotServer {
+    constructor() {
+        this.sensors
+    }
+
+    start(sensors) {
+        this.sensors = sensors
+    }
+
+    publish(action) {
+        const sensor = this.sensors.filter(x => x.deviceId === action.deviceId)[0]
+        if (action.actionId === 'CHANGE_REPORTING_INTERVAL' && sensor.powerStatus === 'on') {
+            sensor.reportingInterval = action.payload 
+        }
+    }
+}
+
+const sensor = new Sensor('id1');
+sensor.turn('on');
+const server = new IotServer();
+server.start([sensor]);
+server.publish({
+    deviceId: 'id1',
+    actionId: 'CHANGE_REPORTING_INTERVAL',
+    payload: 3000,
+});
 
 module.exports = {
     Sensor,
