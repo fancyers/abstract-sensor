855a81e4bb365eadc5757e65e624600377119067
fancyers <kjng5901@gmail.com>
2022-02-07 21:13:43 +0900

    센서의 행동이 반복되고, 전원을 끄면 중단되도록 설정

--- src/index.js
+++ src/index.js
@@ -3,27 +3,36 @@ class Sensor {
         this.deviceId = deviceId
         this.powerStatus = 'off';
         this.reportingInterval = 10000;
+        this.timeOut
     }
 
     turn(input) {
         if (input === 'on') {
             if (this.powerStatus === 'on') {
-                throw Error();
+                throw new Error();
             }
             this.powerStatus = 'on';
             this.status = 'idle';
-            setTimeout(() => {
-                this.status = 'sensingDistance';
-                setTimeout(() => {
-                    this.status = 'reportingData';
-                    setTimeout(() => {
-                        this.status = 'idle';
-                    }, 1000);
-                }, 500);
-            }, this.reportingInterval);
+            this.statusIdle()
         } else if (input === 'off') {
             this.powerStatus = 'off';
+            clearTimeout(this.timeOut)
         }
+    }
+
+    senseDistance() {
+        this.status = 'sensingDistance'
+        this.timeOut = setTimeout(() => { this.reportData() }, 500)
+    }
+
+    reportData() {
+        this.status = 'reportingData'
+        this.timeOut= setTimeout(() => { this.statusIdle() }, 1000)
+    }
+
+    statusIdle() {
+        this.status = 'idle'
+        this.timeOut = setTimeout(() => { this.senseDistance() }, this.reportingInterval)
     }
 }
 
@@ -38,6 +47,7 @@ class IotServer {
 
     publish(action) {
         const sensor = this.sensors.filter(x => x.deviceId === action.deviceId)[0]
+        // 센서 켜져 있을 때, reporting interval 변경하기
         if (action.actionId === 'CHANGE_REPORTING_INTERVAL' && sensor.powerStatus === 'on') {
             sensor.reportingInterval = action.payload 
         }
